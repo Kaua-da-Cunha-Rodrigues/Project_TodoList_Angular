c16fa1b4a1d6b336a2c954f97ba835dadc608f6e
Keo Coelho de Almeida <keo.almeida@totvs.com.br>
2024-04-28 14:39:35 -0300

    Erro NG8001 RESOLVIDO.

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    HomeComponent
+  ]
 })
 
 export class AppComponent {
